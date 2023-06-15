e1cb9dda49ab71b7168d73277c95dfabed2828a1
Jay King <jayriverking@gmail.com>
2023-06-15 13:59:41 -0400

    this works

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,13 +2,25 @@ import './Timeline.css';
 // import timelineData from '..data/timeline.json';
 import TimelineEvent from './TimelineEvent';
 import TimelineData from '../data/timeline.json'
+import TimeStamp from './TimeStamp';
 
 
 const Timeline = () => {
     const dataArray = TimelineData.events;
+    console.log(dataArray);
     const timeComponents = dataArray.map(event => {
-        return <section><TimelineEvent person={event.person} status={event.status} timestamp={event.timestamp} /></section>
+        // const timeStamp = <TimeStamp time={event.timestamp} />
+        return <section>
+            
+            <TimelineEvent
+                person={event.person}
+                status={event.status}
+                time={event.timestamp}>
+            </TimelineEvent>
+
+            </section>
     });
+
     return(
         <div>
             <h1>Time components!</h1>
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -1,12 +1,12 @@
 import './TimelineEvent.css';
 import TimeStamp from './TimeStamp';
 
-const TimelineEvent = ({ person, status, timestamp }) => {
+const TimelineEvent = ({ person, status, time }) => {
     return(
     <div>
         <h5>{person}</h5>
         <p>{status}</p>
-        <p>{timestamp}</p>
+        <TimeStamp time={time}></TimeStamp>
     </div>);
 };
 
